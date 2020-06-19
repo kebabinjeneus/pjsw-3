877f3a2e986e25919f69602dfe3d0731f70c31b2
Lars van der Wijden <sral8991@gmail.com>
2020-06-19 13:21:10 +0200

    het werkt niet

---
+++ src/headers/sensoren.h
@@ -0,0 +1,20 @@
+#ifndef SENSOREN_H
+#define SENSOREN_H
+
+// nodig in main
+//void SENSOREN_init();
+//void sensor_update();
+
+// uphil & gyro
+//void TWI_init();
+
+// uphill vereisten
+void initAccelero(uint8_t A_adresSLAW, uint8_t A_adresSLAR);
+uint8_t readAcceleroVars(uint8_t adresSLAW, uint8_t adresSLAR);
+
+// gyro vereisten
+//void initGyro(uint8_t G_adresSLAW, uint8_t G_adresSLAR);
+uint8_t readGyroVars(uint8_t adresSLAW, uint8_t adresSLAR);
+int32_t getZTotalTurn();
+
+#endif
--- src/main.c
+++ src/main.c
@@ -9,32 +9,47 @@
 #include "headers/usart.h"
 #include "headers/motor.h"
 #include "headers/encoder.h"
+//#include "headers/sensoren.h"
+//void SENSOREN_init();
+//void sensor_update();
+#include "headers/uphill.h"
+#include "headers/gyro.h"
 
 int main() {
 	// initialisaties
 	USART_init();
 	MOTOR_init();
 	ENCODER_init();
+	//SENSOREN_init();
+	//writeString("Ready to play!");
 	
 	// init knopjes
-	// A
-	DDRB &= ~(1 << PORTB3);	// Portb3 input
-	PORTB |= 1 << PORTB3;	// pullup portb3
+	// A & C
+	DDRB &= ~(1 << PORTB3) && ~(1 << PORTB0);	// Portb3 & Portb0 input
+	PORTB |= (1 << PORTB3) | (1 << PORTB0);	// pullup portb3
 	PCICR |= 1 << PCIE0;	// interrupt aanzetten
 	PCMSK0 |= 1 << PCINT3;	// ^^
 	sei();
+	// B
+	DDRD &= ~(1 << PORTD5);
+	PORTD |= (1 << PORTD5);
 
 	// main loop
 	while(1) {
 		test10cm(0); // test of de robot 10cm moet rijden	
 		motorenAanzetten(); // test of motoren aanmoeten volgens toetsenbord
-
-//		writeInt(getEncoderLeft());
-//		writeString(";");
-//		writeInt(getEncoderRight());
-//		writeString(" | ");
+		/*sensor_update();
+		if(~PIND & (1 << PORTD5)) {
+			//sensor_update();
+			//writeString("KNOP B\t");
+			uphill();
+		}*/
+		/*if(~PINB & (1 << PORTB0)) {
+			//writeString("KNOP C\t");
+			//sensor_update();
+			gyro();
+		}*/
 	}
 
 	return 0;
 }
-
