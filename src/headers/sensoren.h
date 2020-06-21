f54f855cef9a02d14563b2d15a2967890a59344a
Lars van der Wijden <sral8991@gmail.com>
2020-06-21 17:18:06 +0200

    header files vergeten

--- src/headers/sensoren.h
+++ src/headers/sensoren.h
@@ -1,23 +1,11 @@
 #ifndef SENSOREN_H
 #define SENSOREN_H
 
-// nodig in main
-//void SENSOREN_init();
-//void sensor_update();
-
-// uphil & gyro
-//void TWI_init();
-
-// uphill vereisten
-//void initAccelero(uint8_t A_adresSLAW, uint8_t A_adresSLAR);
-//uint8_t readAcceleroVars(uint8_t adresSLAW, uint8_t adresSLAR);
-
-// gyro vereisten
+// gyro
 void initGyro(uint8_t G_adresSLAW, uint8_t G_adresSLAR);
 uint8_t readGyroVars(uint8_t adresSLAW, uint8_t adresSLAR);
 int32_t getZTotalTurn();
 void printGyroValues();
-void readGyroValues();
 void TWI_init();
 void gyro();
 
